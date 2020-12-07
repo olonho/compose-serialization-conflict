359afa112bd0ebb54860735c8da11bb73ba775ac
Nikolay Igotti <igotti@gmail.com>
2020-12-07 18:08:10 +0300

    Update Kotlin/Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
 import org.jetbrains.compose.compose
 
 plugins {
-    kotlin("jvm") version "1.4.10"
-    kotlin("plugin.serialization") version "1.4.10"
-    id("org.jetbrains.compose") version "0.1.0-m1-build62"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.serialization") version "1.4.21"
+    id("org.jetbrains.compose") version "0.3.0-build133"
     application
 }
 
 repositories {
     jcenter()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlin/dev")
 }
 
 dependencies {
-    implementation(compose.desktop.all)
+    implementation(compose.desktop.currentOs)
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
 }
 
